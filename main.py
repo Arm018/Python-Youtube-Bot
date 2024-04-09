bfe507add0627a5601c58731991b10d20c333566
Arm018 <arm0182626@gmail.com>
2024-04-09 04:07:54 +0400

    Telegram bot for converting youtube link into mp3

--- main.py
+++ main.py
@@ -1,22 +1,73 @@
-numbers = input("Enter a list of numbers separated by spaces ")
+import os
+import logging
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from pytube import YouTube
+from pydub import AudioSegment
 
-numList = [int(num) for num in numbers.split()]
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+TOKEN = "6420918352:AAEPwQynSVtawUl1ZPEggFhYrNmHr_8kKG0"
 
+DOWNLOAD_FOLDER = './'
 
-
-def classify_numbers(numList):
-    evenNumbers = []
-    oddNumbers = []
-    for num in numList:
-        if num % 2 == 0:
-            evenNumbers.append(num)
-        else:
-            oddNumbers.append(num)
-    return evenNumbers, oddNumbers
+os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
 
-evenNumbers, oddNumbers = classify_numbers(numList)
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Hello! Send me a YouTube link and I will convert it to MP3 for you.')
 
-print(evenNumbers)
-print(oddNumbers)
+
+def download_audio(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+
+
+    if update.message.text and ('youtube.com' in update.message.text or 'youtu.be' in update.message.text):
+
+        try:
+
+            youtube_url = update.message.text
+            yt = YouTube(youtube_url)
+            video_stream = yt.streams.filter(only_audio=True).first()
+            video_stream.download(DOWNLOAD_FOLDER)
+
+
+            mp3_file_path = os.path.join(DOWNLOAD_FOLDER, f"your-song.mp3")
+            audio = AudioSegment.from_file(video_stream.default_filename)
+
+
+            audio = audio.set_frame_rate(48000).set_channels(2).set_sample_width(2)
+
+
+            audio.export(mp3_file_path, format="mp3", bitrate="320k")
+
+
+            context.bot.send_audio(chat_id=chat_id, audio=open(mp3_file_path, 'rb'))
+
+            os.remove(video_stream.default_filename)
+            os.remove(mp3_file_path)
+
+        except Exception as e:
+            logging.error(f"Error processing YouTube link: {e}")
+            update.message.reply_text("Error processing YouTube link. Please try again.")
+
+    else:
+        update.message.reply_text("Please provide a valid YouTube link.")
+
+
+def main() -> None:
+    updater = Updater("6420918352:AAEPwQynSVtawUl1ZPEggFhYrNmHr_8kKG0")
+
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, download_audio))
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
